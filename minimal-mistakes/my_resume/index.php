1eb2f8e1366108fdb9ca719f5e175f1ecd5b4285
The Odd Blog | Digital Ocean <digitalocean@theoddblog.in>
2020-01-21 23:28:05 -0700

    switched to gem-based theme

---
+++ minimal-mistakes/my_resume/index.php
@@ -0,0 +1,12 @@
+<?php
+$file = 'my_resume.pdf';
+$filename = 'Arpit\'s Resume'; /* Note: Always use .pdf at the end. */
+
+header('Content-type: application/pdf');
+header('Content-Disposition: inline; filename="' . $filename . '"');
+header('Content-Transfer-Encoding: binary');
+header('Content-Length: ' . filesize($file));
+header('Accept-Ranges: bytes');
+
+@readfile($file);
+?>
